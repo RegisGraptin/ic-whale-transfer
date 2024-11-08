b0ba1b4f9f2dbe4f4dc414ade7077a014417f095
rere <regis-graptin@sfr.fr>
2024-11-08 22:40:10 +0700

    feat: simple whale nft

--- contract/src/WhaleNFT.sol
+++ contract/src/WhaleNFT.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract WhaleNFT {
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract WhaleNFT is ERC721 {
     
+    uint256 private _nextTokenId;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    constructor() ERC721("WhaleNFT", "WNFT") {}
+    
+    function newWhale(address whaleAddress) public returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _mint(whaleAddress, tokenId);
+        return tokenId;
     }
-
-    function increment() public {
-        number++;
-    }
+    
 }
