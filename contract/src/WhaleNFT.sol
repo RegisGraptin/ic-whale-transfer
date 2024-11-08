b0a5aa9055976c0697591e25736289f61ebaaf77
rere <regis-graptin@sfr.fr>
2024-11-08 22:36:35 +0700

    wip

--- contract/src/WhaleNFT.sol
+++ contract/src/WhaleNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract WhaleNFT {
+    
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
